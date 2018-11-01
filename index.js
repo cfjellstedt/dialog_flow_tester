67dbce584fcd330ae5b880fe19ba18564eb28e4d
cfjellstedt <43853419+cfjellstedt@users.noreply.github.com>
2018-11-01 09:43:28 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -13,20 +13,14 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
+restService.post("/webhook", function(req, res) {
+
+	let text = 'tester';
+	
+return res.json({
+    speech: text
   });
 });
-
 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
