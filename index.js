0d6d5e40924f0171dbc8876b6e36bd4629587ac2
cfjellstedt <43853419+cfjellstedt@users.noreply.github.com>
2018-11-01 13:50:35 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function callThingApi () {
         let last = response['field1'];
 
         // Create response
-        let output = `test`;
+        let output = 'test';
 
         // Resolve the promise with the output text
         console.log(output);
@@ -57,8 +57,11 @@ function callThingApi () {
       });
       res.on('error', (error) => {
         console.log(`Errorinapi`)
-        reject();
+		let output = 'test';
+		resolve(output);
+        //reject();
       });
     });
   });
 }
+
