79216cf166a3e25ace344c2b14ebc673e7ae2bba
cfjellstedt <43853419+cfjellstedt@users.noreply.github.com>
2018-11-01 10:43:48 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const http = require('http');
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const restService = express();
+
+const host = 'api.thingspeak.com';
 
 restService.use(
   bodyParser.urlencoded({
@@ -23,3 +26,35 @@ return res.json({ 'fulfillmentText': `just a test!` });
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
+
+function callThingApi () {
+  return new Promise((resolve, reject) => {
+    // Create the path for the HTTP request to get the weather
+    let path = '/channels/594032/feeds.json?results=2';
+	  
+    console.log('API Request: ' + host + path);
+
+    // Make the HTTP request to get the weather
+    http.get({host: host, path: path}, (res) => {
+      let body = ''; // var to store the response chunks
+      res.on('data', (d) => { body += d; }); // store each response chunk
+      res.on('end', () => {
+        // After all the data has been received parse the JSON for desired data
+        let response = JSON.parse(body);
+        let last = response['field1'];
+  
+
+        // Create response
+        let output = last;
+
+        // Resolve the promise with the output text
+        console.log(output);
+        resolve(output);
+      });
+      res.on('error', (error) => {
+        console.log(`Error calling the weather API: ${error}`)
+        reject();
+      });
+    });
+  });
+}
