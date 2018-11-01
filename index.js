53cf53f4a404f8f7bdcdb26341c6fefa13a56166
cfjellstedt <43853419+cfjellstedt@users.noreply.github.com>
2018-11-01 09:33:54 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 'use strict';
 
-/*
-const host = 'api.worldweatheronline.com';
-const wwoApiKey = '<ENTER_WWO_API_KEY_HERE>';
-*/
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -19,52 +14,11 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/webhook", function(req, res) {
-	
-    //let test = req.body.queryResult.parameters['geo-city'];
-	// res.json({ 'fulfillmentText': `a test` });
+
 	let text = 'tester';
 	
 return res.json({
-    speech: tester
+    speech: text
   });
 });
 
-/*
-function thingApi (city, date) {
-  return new Promise((resolve, reject) => {
-    // Create the path for the HTTP request to get the weather
-    let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
-      '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
-    console.log('API Request: ' + host + path);
-
-    // Make the HTTP request to get the weather
-    http.get({host: host, path: path}, (res) => {
-      let body = ''; // var to store the response chunks
-      res.on('data', (d) => { body += d; }); // store each response chunk
-      res.on('end', () => {
-        // After all the data has been received parse the JSON for desired data
-        let response = JSON.parse(body);
-        let forecast = response['data']['weather'][0];
-        let location = response['data']['request'][0];
-        let conditions = response['data']['current_condition'][0];
-        let currentConditions = conditions['weatherDesc'][0]['value'];
-
-        // Create response
-        let output = `Current conditions in the ${location['type']} 
-        ${location['query']} are ${currentConditions} with a projected high of
-        ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
-        ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
-        ${forecast['date']}.`;
-
-        // Resolve the promise with the output text
-        console.log(output);
-        resolve(output);
-      });
-      res.on('error', (error) => {
-        console.log(`Error calling the weather API: ${error}`)
-        reject();
-      });
-    });
-  });
-}
-*/
